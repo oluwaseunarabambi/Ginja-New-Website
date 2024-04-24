5df75acc2b2fc5eb0161bf5c4cf701b9a306d54d
Arabambi Oluwaseun <161367500+oluwaseunarabambi@users.noreply.github.com>
2024-04-24 04:48:49 -0300

    Add files via upload

--- src/Ai-News.jsx
+++ src/Ai-News.jsx
@@ -1,109 +1,134 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import './styles/Styles.css';
 import { useSpeechSynthesis } from 'react-speech-kit';
 import About from "./About";
 import Contact from "./Contact";
 
-
 const AIWebsite = () => {
     const [articles, setArticles] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
-    const [filterdArticles, setFilteredArticles] = useState([]);
-    const [darkMode, setDarkMode] = useState(false); // state for dark mode
-    const [sortBy, setSortBy] = useState('newest'); // Default sort b newest
-    const [filterBy, setFilterBy] = useState(''); // Default no filter 
-    const [currentPage, setCurrentPage] = useState('');
-    const [itemsPerPage] = useState(10); // Change this to adjust the number of items per page
-
+    const [filteredArticles, setFilteredArticles] = useState([]);
+    const [darkMode, setDarkMode] = useState(false);
+    const [sortBy, setSortBy] = useState('newest');
+    const [filterBy, setFilterBy] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(10);
     const { speak } = useSpeechSynthesis();
-
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        // Fetch news articles from the API
-        fetch('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=UNfyrtbqwbManXET5x0Pb0Bke3UluwrE')
+        fetchArticles();
+    }, []);  // Empty dependency array ensures useEffect runs only after initial render
+
+    const fetchArticles = () => {
+        fetch('https://newsdata.io/api/1/news?apikey=pub_425732c27ef1eeef1d66e821b6dbed936f031&q=Nigeria&country=ng  ')
             .then(response => response.json())
             .then(data => {
-                // Set the fetched articles to the state
-                // Modify the structure of articles to include multimedia content
-                console.log("API Response:", data);
-                const modifiedArticles = data.results.map(article => ({
-                    ...article,
-                    multimedia: {
-                        image: article.multimedia.length > 0 ? article.multimedia[0].url : null, // Assuming image URL is provided in urlToImage field
-                        video: null,  // You can fetch video URLs from another source if available
-                    }
-                }));
-                console.log("Modified Articles:", modifiedArticles);
-                 setArticles(modifiedArticles);
-            })     
+                console.log(data); 
+                if (data && data.results) {
+                    const modifiedArticles = modifyArticles(data.results);
+                    setArticles(modifiedArticles);
+                } else {
+                    throw new Error("Invalid API response");
+                }
+            })
             .catch(error => {
                 setError(error.message);
-            })
-    }, []); // Empty dependency array ensures useEffect runs only after initial render
+            });
+    };
+
+    // Inside the modifyArticles function
+    const modifyArticles = (results) => {
+        return results.map(article => ({
+            article_id: article.article_id,
+            title: article.title || "Untitled",
+            url: article.link || "#",
+            description: article.description || "No description available",
+            pubDate: article.pubDate || "Unknown",
+            image_url: article.image_url,
+            section: article.category && article.category.length > 0 ? article.category[0] : "Uncategorized",
+        }));
+    };
 
     const toggleDarkMode = () => {
         setDarkMode(!darkMode);
     };
+
     const speakText = (title, description) => {
-        speak( { text: `${title} . ${description}`});
+        speak({ text: `${title}. ${description}` });
     };
 
     const handleSubmit = (event) => {
         event.preventDefault(); // Prevent the form submitting normally
-        // Filter articles based on search term
-        const filtered = articles.filter(article =>
-            article.title.toLowerCase().includes(searchTerm)
-        );
-        console.log('Filtered Articles:', filtered); // log filtred articles to verify correctness
-        setFilteredArticles(filtered);
-        setCurrentPage(1); // Reset to the first page when performing a new search
+        filterArticles();
     };
 
     const handleChange = (event) => {
-        console.log('Search Term:', event.target.value);
         setSearchTerm(event.target.value.toLowerCase());
     };
 
     const handleSortChange = (event) => {
-        // console.log('Sort By:', event.target.value);
         setSortBy(event.target.value);
     };
 
     const handleFilterChange = (event) => {
         setFilterBy(event.target.value);
     };
-    // Change page
-        const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    const filterArticles = () => {   // Filter articles based on search term
+        const filtered = articles.filter(article =>
+            article.title.toLowerCase().includes(searchTerm)
+        );
+        setFilteredArticles(filtered);
+        setCurrentPage(1);  // Reset to the first page when performing a new search
+    };
+
+    const sortArticles = (articles) => {   // Apply sorting and filtering to articles
+        let sortedArticles = [...articles];
+        if (sortBy === 'newest') {
+            sortedArticles.sort((a, b) => new Date(b.published_date) - new Date(a.published_date));
+        } else if (sortBy === 'oldest') {
+            sortedArticles.sort((a, b) => new Date(a.published_date) - new Date(b.published_date));
+        }
+        return sortedArticles;
+    };
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
 
     //Calculate the index of the first and last item to display on the current page
+
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = (searchTerm ? filterdArticles : articles).slice(indexOfFirstItem, indexOfLastItem);
+    const currentItems = (searchTerm ? filteredArticles : articles).slice(indexOfFirstItem, indexOfLastItem);
 
+    const sortedAndFilteredArticles = sortArticles(searchTerm ? filteredArticles : articles).filter(article => {
+        return !filterBy || article.section.toLowerCase() === filterBy.toLowerCase();
+    });
 
-    // Apply sorting and filtering to articles
-    let sortedArticles = [...(searchTerm ? filterdArticles : articles)];
-    if (sortBy === 'newest') {
-        sortedArticles.sort((a, b) => new Date(b.published_date) - new Date(a.published_date));
-    } else if (sortBy === 'oldest') {
-        sortedArticles.sort((a, b) => new Date(a.published_date) - new Date(b.published_date));
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(sortedAndFilteredArticles.length / itemsPerPage); i++) {
+        pageNumbers.push(i);
     }
 
-    if (filterBy) {
-        sortedArticles = sortedArticles.filter(article => 
-            article.section.toLowerCase() === filterBy.toLowerCase()
-        );
-    }
+    // Function to handle sharing article on Twitter
+    const shareOnTwitter = (url) => {
+        window.open(`https://twitter.com/intent/tweet?url=${encodeURIComponet(url)}`);
+    };
 
-    const pageNumbers =[];
-    for (let i =1; i <= Math.ceil((searchTerm ? filterdArticles.length : articles.length) / itemsPerPage); i++) {
-        pageNumbers.push(i)
-    }
+    // Function to handle sharing article on Facebook
+    const shareOnFacebook = (url) => {
+        window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`);
+    };
+
+    // Function to handle sharing article on LinkedIn
+    const shareOnLinkedIn = (url) => {
+        window.open(`https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(url)}`);
+    };
 
     return (
-
-        <div className={darkMode ? "dark-mode" : "light-mode"}> {/* Apply dark/light mode class */}
+        <div className={darkMode ? "dark-mode" : "light-mode"}>
             <header>
                 <h1>Ginja News Website</h1>
                 <nav>
@@ -114,7 +139,7 @@ const AIWebsite = () => {
                     </ul>
                 </nav>
                 <button onClick={toggleDarkMode}>
-                    {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode" }
+                    {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
                 </button>
             </header>
 
@@ -133,78 +158,71 @@ const AIWebsite = () => {
                     <button type="submit">Search</button>
                 </form>
                 <div>
-                     <label htmlFor="sort-select">Sort by:</label>
-                     <select id="sort-select" value={sortBy} onChange={handleSortChange}>
-                          <option value="newest">Newest</option>
-                          <option value='oldest'>Oldest</option>
-                     </select>
-                     <label htmlFor="filter-select">Filter by section:</label>
-                     <select id="filter-select" value={filterBy} onChange={handleFilterChange}>
-                            <option value=" ">All</option>
-                            <option value="world">World</option>
-                            <option value="polictics">Politics</option>
-                            <option value="sports">Sports</option>
-                            <option value="business">Business</option>
-                     </select>
+                    <label htmlFor="sort-select">Sort by:</label>
+                    <select id="sort-select" value={sortBy} onChange={handleSortChange}>
+                        <option value="newest">Newest</option>
+                        <option value='oldest'>Oldest</option>
+                    </select>
+                    <label htmlFor="filter-select">Filter by section:</label>
+                    <select id="filter-select" value={filterBy} onChange={handleFilterChange}>
+                        <option value="">All</option>
+                        <option value="world">Nigeria</option>
+                        <option value="politics">Politics</option>
+                        <option value="sports">Sports</option>
+                        <option value="business">Business</option>
+                        <option value="health">Health</option>
+                        <option value="briefing">Briefing</option>
+                    </select>
                 </div>
             </section>
 
             <section id="latest-headlines">
-                <u><h2>Latest headline News from Around the World</h2></u> 
+                <u><h2>Latest headline News from Around the Nigeria</h2></u>
                 <ul id="articles-list">
                     {currentItems.map((article, index) => (
                         <li key={index}>
-
-                                <h3>
-                                   <a href={article.url} target="_blank" rel="noopener noreferrer">{article.title}</a>
-                                </h3>
-
-                                {article.multimedia.image ? (
-                                    <img src={article.multimedia.image} alt="Article Thumbnail" />
-                                ) : (
-                                    <div className="no-multimedia">Multimedia Not Available</div>
-                                    
-                                )}
-
-                                {/* {article.multimedia.image && <img src={article.multimedia.image} alt="Article Thumbnail" />}
-                                {article.multimedia.video && <video src={article.multimedia.video} controls />} */}
-
-                                <p>{article.abstract}</p>
-
-                                <button className="ReadArticleButton" onClick={ () => speak( { text: article.title + '. ' + article.abstract })}>
-                                            Read Article
-                                </button>
-
-                                    {/* Add a console log to check article URLs */}
-                                    {console.log("Article URL:", article.url)}
+                            <h3>
+                                <a href={article.url} target="_blank" rel="noopener noreferrer">{article.title}</a>
+                            </h3>
+                            <div className="social-media-buttons">
+                                <button onClick={() => shareOnTwitter(article.url)}>Share on Twitter</button>
+                                <button onClick={() => shareOnFacebook(article.url)}>Share on Facebook</button>
+                                <button onClick={() => shareOnLinkedIn(article.url)}>Share on LinkedIn</button>
+                            </div>
+                            {article.urlToImage ? (
+                                <img src={article.urlToImage} alt="Article Thumbnail" />
+                            ) : (
+                                <div className="no-image">Image Not Available</div>
+                            )}
+                            <p>{article.description}</p>
+                            <button className="ReadArticleButton" onClick={() => speak({ text: article.title + '. ' + article.description })}>
+                                Read Article
+                            </button>
                         </li>
                     ))}
                 </ul>
                 <ul id="pagination">
-                      {pageNumbers.map(number =>(
-                           <li key={number} className={currentPage === number ? 'active' : ''}>
-                                <button 
-                                    onClick={ () => paginate(number)}
-                                    aria-label={`Go to page ${number}`}
-                                    aria-current={currentPage === number ? 'page' : null}
-
-                                >
-                                    {number}
-                                </button>
-                           </li>
-                      ))}           
+                    {pageNumbers.map(number => (
+                        <li key={number} className={currentPage === number ? 'active' : ''}>
+                            <button
+                                onClick={() => paginate(number)}
+                                aria-label={`Go to page ${number}`}
+                                aria-current={currentPage === number ? 'page' : null}
+                            >
+                                {number}
+                            </button>
+                        </li>
+                    ))}
                 </ul>
             </section>
-            
-            <About/>
-            <Contact/>
+
+            <About />
+            <Contact />
             <footer>
                 <p>&copy; {new Date().getFullYear()} Ginja Tech AI News Website</p>
             </footer>
         </div>
-
     );
-
 };
 
 export default AIWebsite;
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import {React, useState } from 'react';
 import './App.css';
-import Swr from './Swr';
 import AIWebsite from './Ai-News';
 function App() {
   
